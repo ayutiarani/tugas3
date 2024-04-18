6dc039bb2e5d2cee03b549cabc2142a50b5e3b47
Rizma Sabillah <rizmasabillah02@gmail.com>
2024-04-18 15:31:50 +0700

    tugas 3

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Hallo Rizma Sabillah!'),
+      home: const MyHomePage(title: 'Hallo Ayu Tiarani!'),
     );
   }
 }
